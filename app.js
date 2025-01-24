3ee6b7cb472c0e65f961f32d7b438c4b86d9626f
rabiasoeyle <ing.rabia.soeylemez@gmail.com>
2025-01-24 11:42:14 +0100

    <feat>:<added telegrambot api>

--- app.js
+++ app.js
@@ -48,5 +48,5 @@ const bot = new TelegramBot(token, {polling: true});
 bot.on('message', (msg) => {
 const chatId = msg.chat.id;
   // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
+  bot.sendMessage(chatId, 'Received your message Rabia');
 });
