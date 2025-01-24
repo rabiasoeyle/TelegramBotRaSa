d9eaf433ca6cccc803c8bac4bc64e3ac08c9f02a
rabiasoeyle <141157379+rabiasoeyle@users.noreply.github.com>
2025-01-24 11:21:12 +0100

    Update app.js

--- app.js
+++ app.js
@@ -39,3 +39,14 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+const TelegramBot = require('node-telegram-bot-api');
+// replace the value below with the Telegram token you receive from @BotFather
+const token = '7217632254:AAEHcD9onjWz2y622qYIebtjsW3AocHzzaY';
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+bot.on('message', (msg) => {
+const chatId = msg.chat.id;
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message');
+});
