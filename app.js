6bc4fcf732b8765ce5e192aece7e090bc3757241
rabiasoeyle <ing.rabia.soeylemez@gmail.com>
2025-01-28 09:30:05 +0100

    <feat>:<added special reaction to a specific text>

--- app.js
+++ app.js
@@ -3,50 +3,36 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 var app = express();
-
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
-
 module.exports = app;
-
 const TelegramBot = require('node-telegram-bot-api');
-// replace the value below with the Telegram token you receive from @BotFather
 const token = '7217632254:AAEHcD9onjWz2y622qYIebtjsW3AocHzzaY';
-// Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 bot.on('message', (msg) => {
 const chatId = msg.chat.id;
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message Rabia');
+  if(msg.text === "/start"){
+    bot.sendMessage(chatId, 'Hello Rabia');
+  }else{
+    bot.sendMessage(chatId, 'Received your message Rabia');}
+  
 });
